121a9d90f895bf2cb1ca41bfe18ad0bc626f99c9
Steven Zhang <steven@pursuit.org>
2023-07-18 20:15:03 -0400

    maybe the universe domain is the issue?

--- generateCreds.js
+++ generateCreds.js
@@ -13,6 +13,7 @@ const credentials = {
   token_uri: 'https://oauth2.googleapis.com/token',
   auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
   client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
+  universe_domain: "googleapis.com"
 };
 
 fs.writeFileSync(
