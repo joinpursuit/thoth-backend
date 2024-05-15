8b6e088d35c8a27a359f2316448d9561c395c89f
Steven Zhang <steven@pursuit.org>
2024-05-15 14:51:57 -0400

    Add separate route for login and signup

--- routes/users.js
+++ routes/users.js
@@ -3,37 +3,35 @@ const router = express.Router();
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
+router.post("/login", async (req, res) => {
+  const { firebaseId } = req.body;
+  const user = await prisma.user.findUnique({ where: { firebaseId } });
+  if(user) {
+    return res.json(user);
+  }
+  res.status(404).json({ error: "User not found." });
+})
+
 router.post("/create", async (req, res) => {
   const { firebaseId, email } = req.body;
   
   // Temporary to give new users access to a test class.
   // const demoClass = await prisma.class.findFirst({ where: { name: "Pursuit Alpha Test" } });
   
-  let user = await prisma.user.findUnique({ where: { email } });
-  let membership;
+  let user = await prisma.user.findUnique({ where: { firebaseId } });
 
   if(user) {
-    // membership = await prisma.classMembership.findUnique({ 
-    //   where: { userId_classId: { userId: user.id, classId: demoClass.id }} 
-    // });
+    return res.json(user);
   }
   else {
     user = await prisma.user.create({
       data: {
-        email, firebaseId
+        email,
+        firebaseId
       }
     });
   }
 
-  // TODO: Remove this once we have a real login system.
-  // if(!membership) {
-  //   membership = await prisma.classMembership.create({
-  //     data: {
-  //       user: { connect: { id: user.id } },
-  //       associatedClass: { connect: { id: demoClass.id } }
-  //     }
-  //   });
-  // }
   res.json(user);
 })
 
